470bd0656ad10490f5f2e354e89be51b6a9fc95a
William Dolke <williamdolke@hotmail.co.uk>
2023-04-10 02:33:46 +0100

    Dismiss keyboard with a downwards drag gesture and allow autocorrect

--- HGFcollective/Components/MessageField.swift
+++ HGFcollective/Components/MessageField.swift
@@ -20,22 +20,11 @@ struct MessageField: View {
 
     var body: some View {
         HStack {
-            Button {
-                showImagePicker.toggle()
-                logger.info("User tapped the image picker button")
-            } label: {
-                Image(systemName: "photo.on.rectangle.angled")
-                    .foregroundColor(Color.theme.buttonForeground)
-                    .font(.system(size: 25))
-                    .padding(10)
-                    .background(Color.theme.accent)
-                    .cornerRadius(50)
-            }
+            mediaButton
 
             CustomTextField(text: $message, image: $image, placeholder: Text("Enter your message here"))
                 .padding()
                 .contentShape(Rectangle())
-                .disableAutocorrection(true)
 
             sendButton
         }
@@ -47,6 +36,32 @@ struct MessageField: View {
             // For camera, use .camera
             ImagePickerView(selectedImage: $image, sourceType: .photoLibrary)
         }
+        // Dismiss the keyboard with a downward drag gesture. The user can also dismiss the
+        // keyboard by pressing the 'return' key.
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    // Ignore upward swipes
+                    guard value.translation.height > 0 else { return }
+                }
+                .onEnded { _ in
+                    UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.endEditing(true)
+                }
+        )
+    }
+
+    private var mediaButton: some View {
+        Button {
+            showImagePicker.toggle()
+            logger.info("User tapped the image picker button")
+        } label: {
+            Image(systemName: "photo.on.rectangle.angled")
+                .foregroundColor(Color.theme.buttonForeground)
+                .font(.system(size: 25))
+                .padding(10)
+                .background(Color.theme.accent)
+                .cornerRadius(50)
+        }
     }
 
     private var sendButton: some View {
@@ -54,7 +69,7 @@ struct MessageField: View {
             Button {
                 if message.lowercased() == "admin login" {
                     message = ""
-                    self.showLogin = true
+                    showLogin = true
                     logger.info("User entered the secret keyphrase")
                 } else if message != "" {
                     messagesManager.sendMessage(text: message, type: "text")
@@ -79,12 +94,14 @@ struct MessageField: View {
 }
 
 struct MessageField_Previews: PreviewProvider {
+    static let messagesManager = MessagesManager(uid: "test", notificationName: "test")
+
     static var previews: some View {
         MessageField()
-            .environmentObject(MessagesManager(uid: "test"))
+            .environmentObject(messagesManager)
 
         MessageField()
-            .environmentObject(MessagesManager(uid: "test"))
+            .environmentObject(messagesManager)
             .preferredColorScheme(.dark)
     }
 }
