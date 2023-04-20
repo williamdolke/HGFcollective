d27df22d289c7b3c8d2a585b0f6fe7ac86dedf52
William Dolke <williamdolke@hotmail.co.uk>
2023-04-20 09:17:06 +0100

    Add Notification Service

---
+++ NotificationService/NotificationService.swift
@@ -0,0 +1,31 @@
+//
+//  NotificationService.swift
+//  NotificationService
+//
+//  Created by William Dolke on 17/04/2023.
+//
+
+import UserNotifications
+import FirebaseMessaging
+
+class NotificationService: UNNotificationServiceExtension {
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+        if let bestAttemptContent = bestAttemptContent {
+            FIRMessagingExtensionHelper().populateNotificationContent(
+                bestAttemptContent,
+                withContentHandler: contentHandler)
+        }
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+}
